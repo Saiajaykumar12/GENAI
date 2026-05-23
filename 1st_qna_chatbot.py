5714ffa21e7766c57c33cc127ddf901785185e76
Ajay <saiajay1214@gmail.com>
2026-05-23 14:57:07 +0530

    Successfully added memory to my AI chatbot

--- 1st_qna_chatbot.py
+++ 1st_qna_chatbot.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
 
 from langchain_groq import ChatGroq
 llm = ChatGroq(model="llama-3.3-70b-versatile")
@@ -9,11 +10,32 @@ llm = ChatGroq(model="llama-3.3-70b-versatile")
 st.title("🎉Ask Anything I am here to help you ! 😎")
 st.markdown("I am a GenAI chatbot built using LangChain and Groq's LLM. Ask me anything!")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+for messages in st.session_state.messages:
+    role = messages["role"]
+    content = messages["content"]
+    st.chat_message(role).markdown(content)
+
 query = st.chat_input("Ask me anything!")
 
-if query:
-    print(query)
-    st.chat_message("user").markdown(query)
-    res = llm.invoke(query)
-    st.chat_message("ai").markdown(res.content)
+# if query:
+#     st.session_state.messages.append({"role": "user", "content": query})
+#     st.chat_message("user").markdown(query)
+#     res = llm.invoke(query)
+#     st.chat_message("ai").markdown(res.content)
+#     st.session_state.messages.append({"role":"ai","content":res.content})
 
+
+if query:
+    st.chat_message("user").markdown(query)
+    st.session_state.messages.append({"role": "user","content": query})
+    st.session_state.history.append(HumanMessage(content=query))
+    res = llm.invoke(st.session_state.history)
+    st.chat_message("ai").markdown(res.content)
+    st.session_state.messages.append({"role": "ai","content": res.content})
+    st.session_state.history.append(AIMessage(content=res.content))
